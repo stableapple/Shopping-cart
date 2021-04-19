a61f124859d2d019a4168b2d4f74de5139bd3b2c
stableapple <riddhi01.menon@gmail.com>
2021-04-19 10:33:29 +0530

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import { render } from '@testing-library/react';
+import React, { Component } from 'react';
+
+class App extends React.Component{
+    render() {
+        return(
+            <div>App</div>
+        )
+            
+        
+    }
+}
+export default App;
