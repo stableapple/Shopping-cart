ddc4b813f96be21492fe59b9bd79c7a07d524500
stableapple <riddhi01.menon@gmail.com>
2021-05-09 18:43:27 +0530

    added data

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -7,14 +7,3 @@ export const fetchProducts = () => async dispatch => {
     
 }
 
-export const fetchCart = () => async dispatch => {
-    const response= await json.get('/carts/5')
-    dispatch({type: 'FETCH_CART', payload:response.data})
-}
-
-export const addToCart = (id) => async dispatch => {
-    const value={userId: 3, date: 2019-12-10, products:[{productId: 2, quantity: 3}]}
-    const response= await json.patch('/carts/5', {...value})
-    console.log(response)
-    dispatch({type: 'ADD_CART', payload:response.data})
-}
